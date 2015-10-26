1e63bdc40d3b8c3f261ebf9735a949da86ba31de
Ethan Welsh <ejw45@pitt.edu>
2015-10-26 01:47:02 -0400

    cache results of clustering to improve performance on sucessive runs

--- HW3/reverse-retreival/visualizeVocabulary.m
+++ HW3/reverse-retreival/visualizeVocabulary.m
@@ -23,53 +23,58 @@ for i = (1:frame_sample_size)
     end
 end
 
-[membership, means, ~] = kmeansML(1500, random_sift_indexes');
-[members_len, ~] = size(membership);
+if exist('centers.mat', 'file') == 2
+    load('centers.mat', 'membership', 'means');
+else
+    [membership, means, ~] = kmeansML(1500, random_sift_indexes');
+    save('centers.mat', 'membership', 'means');
+end
 
-words_to_compare_to_find_max_distance = 300;
-random_membership_indexes = randperm(members_len);
-
-max_distance = 0;
-
-w1 = [];
-w2 = [];
-
-for i = (1:words_to_compare_to_find_max_distance)
+if exist('two.mat', 'file') ~= 2
     
-    word1_index = membership(random_membership_indexes(i));
-    
-    [member_count, ~] = size(membership(membership==word1_index));
-    if member_count < 25
-        continue
-    end
-    
-    for j = (1:words_to_compare_to_find_max_distance)
-        
-        word2_index = membership(random_membership_indexes(j));
-        
-        
-        [member_count, ~] = size(membership(membership==word2_index));
+    [members_len, ~] = size(membership);
+
+    words_to_compare_to_find_max_distance = 300;
+    random_membership_indexes = randperm(members_len);
+
+    max_distance = 0;
+
+    w1 = [];
+    w2 = [];
+
+    for i = (1:words_to_compare_to_find_max_distance)
+
+        word1_index = membership(random_membership_indexes(i));
+
+        [member_count, ~] = size(membership(membership==word1_index));
         if member_count < 25
             continue
         end
-        
-        size(random_sift_indexes)
-        
-        word1 = random_sift_indexes(word1_index, :);
-        word2 = random_sift_indexes(word2_index, :);
-        
-        distance_between_words = dist2(word1, word2);
-        
-        if distance_between_words > max_distance
-            max_distance = distance_between_words;
-            w1 = word1;
-            w2 = word2;
+
+        for j = (1:words_to_compare_to_find_max_distance)
+
+            word2_index = membership(random_membership_indexes(j));
+
+
+            [member_count, ~] = size(membership(membership==word2_index));
+            if member_count < 25
+                continue
+            end
+
+            word1 = random_sift_indexes(word1_index, :);
+            word2 = random_sift_indexes(word2_index, :);
+
+            distance_between_words = dist2(word1, word2);
+
+            if distance_between_words > max_distance
+                max_distance = distance_between_words;
+                w1 = word1;
+                w2 = word2;
+            end
         end
     end
+    save('two.mat', 'w1', 'w2');
+else
+   load('two.mat', 'w1', 'w2'); 
 end
-
-w1
-w2
-
-
 
