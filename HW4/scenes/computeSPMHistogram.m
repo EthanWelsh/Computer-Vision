59a47c2b0205d2f98cc9f3c36c8e0e5c22bdc348
Ethan Welsh <welshej@gmail.com>
2015-11-22 13:11:33 -0500

    build level0 and level1 of pyramid

--- HW4/scenes/computeSPMHistogram.m
+++ HW4/scenes/computeSPMHistogram.m
@@ -1,44 +1,38 @@
 function [pyramid] = computeSPMHistogram(im, codebook_centers)
     
+    [height, width] = size(im);
+
     means = codebook_centers';
     [frames, descriptors] = vl_sift(single(im));
-    histogram = getHistogram(descriptors, means);
-
-    length(frames)
-    length(descriptors)
+        
+    level_0 = getHistogram(descriptors, means);
     
     q1_descriptors = [];
     q2_descriptors = [];
     q3_descriptors = [];
     q4_descriptors = [];
     
-    %frames
-    %size(frames)
-    %length(frames)
-    
-    
     for i = (1 : length(frames))
-        x = frames(1, i);
-        y = frames(2, i);
+        x = (frames(1, i)/width) * 100.00;
+        y = (frames(2, i)/height) * 100.00;
         
         if x <= 50 && y <= 50
-            q1_descriptors = [q1_descriptors; descriptors(i)];
+            q1_descriptors = [q1_descriptors descriptors(:, i)];
         elseif x > 50 && y <= 50
-            q2_descriptors = [q2_descriptors; descriptors(i)];
+            q2_descriptors = [q2_descriptors descriptors(:, i)];
         elseif x <= 50 && y > 50
-            q3_descriptors = [q3_descriptors; descriptors(i)];
+            q3_descriptors = [q3_descriptors descriptors(:, i)];
         elseif x > 50 && y > 50
-            q4_descriptors = [q4_descriptors; descriptors(i)];
+            q4_descriptors = [q4_descriptors descriptors(:, i)];
         end
     end
+        
+    level_1 = [getHistogram(q1_descriptors, means) getHistogram(q2_descriptors, means) getHistogram(q3_descriptors, means) getHistogram(q4_descriptors, means)];
     
-    size(q1)
-    size(q2)
-    size(q3)
-    size(q4)
+    size(level_0)
+    size(level_1)
     
-    pyramid = [];
+    pyramid = [level_0 level_1];
+    size(pyramid)
    
-
-
 end
