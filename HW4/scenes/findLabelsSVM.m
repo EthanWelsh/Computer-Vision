330ed5af49580b73235c3652cbaf43cca6553d92
Ethan Welsh <welshej@gmail.com>
2015-11-23 08:59:10 -0500

    SVM

--- HW4/scenes/findLabelsSVM.m
+++ HW4/scenes/findLabelsSVM.m
@@ -1,2 +1,15 @@
 function [labels] = findLabelsSVM(pyramids_train, pyramids_test, labels_train)
+    
+    train = [];
+   
+    for i = (1 : length(pyramids_train))
+        train = [train; pyramids_train{i}(:)'];
+    end
+    
+    model = fitcecoc(train, labels_train);
+    labels = {};
+    
+    for i = (1 : length(pyramids_test))
+        labels{i} = predict(model, pyramids_test{i}(:)');
+    end
 end
